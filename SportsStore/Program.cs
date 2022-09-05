12347328cb096e2d8d39c0d4bb487071ee927855
Barry Wallis <barry.wallis@outlook.com>
2022-09-05 14:51:25 -0700

    Add project files.

---
+++ SportsStore/Program.cs
@@ -0,0 +1,7 @@
+﻿WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+WebApplication app = builder.Build();
+
+app.UseStaticFiles();
+app.MapDefaultControllerRoute();
+app.Run();
