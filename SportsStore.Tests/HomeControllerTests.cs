6a0b0dd9432fd658b5a0cfb503c04e3cb2b2a154
Barry Wallis <barry.wallis@outlook.com>
2022-09-05 17:32:48 -0700

    Displaying a list of products.

---
+++ SportsStore.Tests/HomeControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Moq;
+
+using SportsStore.Controllers;
+using SportsStore.Models;
+
+namespace SportsStore.Tests;
+public class HomeControllerTests
+{
+    [Fact]
+    public void CanUseRepository()
+    {
+        Product[] expected = new Product[]
+        {
+            new Product { ProductId = 1, Name = "P1" },
+            new Product { ProductId = 2, Name = "P2" }
+        };
+        Mock<IStoreRepository> mock = new();
+        _ = mock.Setup(sr => sr.Products).Returns(expected.AsQueryable());
+        HomeController homeController = new(mock.Object);
+
+        IEnumerable<Product>? actual = (homeController.Index() as ViewResult)?.ViewData
+                                                                              .Model as IEnumerable<Product>;
+
+        Product[] products = actual?.ToArray() ?? Array.Empty<Product>();
+        Assert.Equal(expected, products);
+    }
+}
--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using SportsStore.Models;
+
 namespace SportsStore.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index() => View();
+    private readonly IStoreRepository _storeRepository;
+
+    public HomeController(IStoreRepository storeRepository) => _storeRepository = storeRepository;
+
+    public IActionResult Index() => View(_storeRepository.Products);
 }
